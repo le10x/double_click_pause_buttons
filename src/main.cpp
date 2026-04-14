1d64d9bcd00e5b1ded5775828522d9ef756e415b
le10x <leanderdrb@gmail.com>
2026-04-13 20:05:43 -0400

    Implement player frame scaling in updatePlayerFrame
    Modify PlayerObject to update player frame scaling based on orientation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayerObject.hpp>
 
+using namespace geode::prelude;
+
+class $modify(PlayerObject) {
+    void updatePlayerFrame() {
+        PlayerObject::updatePlayerFrame();
+
+        if (this->m_isSwing) {
+            if (this->m_isUpsideDown) {
+                this->setScaleY(-1.0f);
+            } else {
+                this->setScaleY(1.0f);
+            }
+        }
+    }
+};
